96e21e0b98bcdaf3712a43c6ab74b9016fc31848
NITRO5\daniel <daniel@softinline.com>
2023-04-16 16:44:30 +0200

    add: icon support for button

--- src/views/components/button.blade.php
+++ src/views/components/button.blade.php
@@ -1,9 +1,13 @@
 <?php
     // prepare action link
     $tmp = explode(':', $component['action']);
+    $icon = "";
+    if(isset($component['icon'])) {
+        $icon = '<i class="'.$component['icon'].'"></i> ';
+    }
 ?>
 <?php if($tmp[0] == 'js') { ?>
-    <button type="button" class="{{ $component['class'] }}" onclick="{{ $tmp[1] }}">{{ ucfirst(trans('messages.'.$component['title'])) }}</button>        
+    <button type="button" class="{{ $component['class'] }}" onclick="{{ $tmp[1] }}">{!! $icon !!}{{ ucfirst(trans('messages.'.$component['title'])) }}</button>        
 <?php } else { ?>
-    <button type="button" class="{{ $component['class'] }}" onclick="window.open('{{ url($tmp[1]) }}','_self')">{{ ucfirst(trans('messages.'.$component['title'])) }}</button>
+    <button type="button" class="{{ $component['class'] }}" onclick="window.open('{{ url($tmp[1]) }}','_self')">{!! $icon !!}{{ ucfirst(trans('messages.'.$component['title'])) }}</button>
 <?php } ?>
