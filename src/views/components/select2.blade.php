10e33fcaff1e7c3edc9519b3e4837d1cbc70d19b
NITRO5\daniel <daniel@softinline.com>
2023-06-05 12:49:28 +0200

    add: added select2 component

---
+++ src/views/components/select2.blade.php
@@ -0,0 +1,63 @@
+<?php 
+
+    // options
+    $method = $component['selector'];
+    $options = $controller::$method(@$item, @$id);
+
+    // field
+    $field = $component['field'];
+
+    // title
+    $title = $component['title'];
+
+    // required
+    $required = false;
+    if(isset($component['required'])) {
+        $required = $component['required'];
+    }
+
+    // disabled
+    $disabled = false;
+    if(isset($component['disabled'])) {
+        $disabled = $component['disabled'];
+    }
+
+    // class
+    $class = '';
+    if(isset($component['class'])) {
+        $class = $component['class'];
+    }
+
+?>
+<div class="form-group">
+    <label>{{ ucfirst(trans('messages.'.$title)) }}: {{ $required ? '*' : '' }}</label>
+    <select name="{{ $field }}" id="{{ $field }}" class="form-control sfwcomponent-select2 {{ $class }} {{ $required ? 'sfwcomponent-frm-item-required' : '' }}" {{ $required ? 'required' : '' }} {{ $disabled ? 'disabled' : '' }} sfwcomponent-data-title="{{ ucfirst(trans('messages.'.$title)) }}">
+        <option value="">{{ ucfirst(trans('messages.select-option')) }}</option>
+
+        <?php foreach($options as $option) { ?>
+        <optgroup label="{{$option['title']}}">
+            <?php foreach($option['options'] as $optionKey => $optionValue) { ?>
+                <?php
+                    $selected = '';  
+                    // determine default value
+                    // if edit, dont take effect
+                    // on add check if Request has a param with this value    
+                    if(@$item) {
+                        $selected = $optionKey == @$item->{$field} ? 'selected' : '';
+                    }
+                    else {
+                        $selected = $optionKey == \Request::get($field) ? 'selected' : '';
+                    }
+                ?>
+                <option value="{{ $optionKey }}" {{ $selected }}>{{ ucfirst($optionValue) }}</option>
+            <?php } ?>
+        </optgroup>
+        <?php } ?>
+
+    </select>
+</div>
+<script>
+    $(document).ready(function() {
+        $('.sfwcomponent-select2').select2();
+    });
+</script>
